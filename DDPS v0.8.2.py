d3d11043fc8afaf9149387ef89875d23a2e9671e
ISRT5I <33969724+ISRT5I@users.noreply.github.com>
2017-11-29 02:24:31 -0600

    Updated DDPS.py to v0.8.2
    Implemented white-listing, as well as memory scan to later be used for real-time scans. Commented out code that is incomplete. 
    Next plans: Implementing real-time scanning (possibly using threads) and file-type options

--- DDPS v0.8.2.py
+++ DDPS v0.8.2.py
@@ -1,70 +1,82 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 from tkinter import *
-import os, sys
+import os, sys, time
 import hashlib
 
 class Scroll(object):
 
-    def __init__(self):
-        self.root = tk.Tk()
-    # create a Text widget with a Scrollbar attached
-        self.txt = ScrolledText(self.root, undo=True)
-        self.txt['font'] = ('consolas', '12')
-        self.txt.pack(expand=True, fill='both')
+#    def __init__(self):
+#        self.root = tk.Tk()
+#    # create a Text widget with a Scrollbar attached
+#        self.txt = ScrolledText(self.root, undo=True)
+#        self.txt['font'] = ('consolas', '12')
+#        self.txt.pack(expand=True, fill='both')
 
-class GUI:
-
-    def __init__(self, master):
-       frame = Frame(master)
-       frame.pack()
-       self.menuButton = Button(frame, text="Menu", height=5, width=10)
-       self.menuButton.grid(row=0, sticky=W)
-       self.fileButton = Button(frame, text="Files", height=5, width=10)
-       self.fileButton.grid(row=1, sticky=W)
-       self.dupButton = Button(frame, text="Duplicates", height=5, width=10)
-       self.dupButton.grid(row=2, sticky=W)
-       self.setButton = Button(frame, text="Settings", height=5, width=10)
-       self.setButton.grid(row=3, sticky=W)
-       self.scanButton = Button(frame, text="Scan", bg="blue", fg="white", height=3,width=15)
-       self.scanButton.grid(row=3, column=5, sticky=E)
-       self.dupLabel = Label(frame, text="Duplicates: ")
-       self.dupLabel.grid(row=2, column=3, sticky=W)
+#class GUI:
+#
+#    def __init__(self, master):
+#       frame = Frame(master)
+#       frame.pack()
+#       self.menuButton = Button(frame, text="Menu", height=5, width=10)
+#       self.menuButton.grid(row=0, sticky=W)
+#       self.fileButton = Button(frame, text="Files", height=5, width=10)
+#       self.fileButton.grid(row=1, sticky=W)
+#       self.dupButton = Button(frame, text="Duplicates", height=5, width=10)
+#       self.dupButton.grid(row=2, sticky=W)
+#       self.setButton = Button(frame, text="Settings", height=5, width=10)
+#       self.setButton.grid(row=3, sticky=W)
+#       self.scanButton = Button(frame, text="Scan", bg="blue", fg="white", height=3,width=15)
+#       self.scanButton.grid(row=3, column=5, sticky=E)
+#       self.dupLabel = Label(frame, text="Duplicates: ")
+#       self.dupLabel.grid(row=2, column=3, sticky=W)
        
 class Hash:
-#findDup
-    def memDup(parentFolder):
+
+    def findDup(parentFolder):
         dups = {}
-        for dirName, subdirs, fileList in os.walk(parentFolder):
+        for dirs, subdirs, fileList in os.walk(parentFolder):
+            dirName = dirs.replace("\\", "/")
             print('Scanning %s...' % dirName)
             for filename in fileList:
-                path = os.path.join(dirName, filename)
+                path = dirName + "/" + filename
                 file_hash = Hash.hashfile(path)
                 if file_hash in dups:
                     dups[file_hash].append(path)
                 else:
                     dups[file_hash] = [path]
         return dups
-#memDup    
-    def findDup(parentFolder):
+  
+    def memDup(parentFolder):
         dups = {}
         dic = open(os.path.join('lists', "dictionary.txt"),"a+")
         dic.seek(0)
         dicts = [line.split("|") for line in dic]
-        for dirName, subdirs, fileList in os.walk(parentFolder):
-            print('Scanning %s...' % dirName)
-            for filename in fileList:
-                path = os.path.join(dirName, filename)
-                file_hash = Hash.hashfile(path)
-                if file_hash in dups:
-                    dups[file_hash].append(path)
-                elif any(file_hash in sublist for sublist in dicts):
-                    dups[file_hash] = [path]
-                else:
-                    dic.write(file_hash + "|" + path + "\n")
-                    dicts.append([file_hash , path])
-                    dups[file_hash] = [path]
-        dic.close
+        wFiles = open(os.path.join('lists', "whitefile.txt"),"a+")
+        wFiles.seek(0)
+        whiteFiles = [x.strip() for x in wFiles.readlines()]
+        wFolder = open(os.path.join('lists', "whitefolder.txt"),"a+")
+        wFolder.seek(0)
+        whiteFolder = [x.strip() for x in wFolder.readlines()]
+        for dirs, subdirs, fileList in os.walk(parentFolder):
+            dirName = dirs.replace("\\", "/")
+            if dirName not in whiteFolder:
+                print('Scanning %s...' % dirName)
+                for filename in fileList:
+                    path = dirName + "/" + filename
+                    if path not in whiteFiles:
+                        file_hash = Hash.hashfile(path)
+                        if file_hash in dups:
+                            dups[file_hash].append(path)
+                        elif any(file_hash in sublist for sublist in dicts):
+                            dups[file_hash] = [path]
+                        else:
+                            dic.write(file_hash + "|" + path + "\n")
+                            dicts.append([file_hash , path])
+                            dups[file_hash] = [path]
+        dic.close()
+        wFiles.close()
+        wFolder.close()
         return dups
 
     def joinDicts(dict1, dict2):
@@ -100,7 +112,25 @@ class Hash:
         else:
             messagebox.showerror("DDPS", "No duplicate files found.")
     
-    def Scan():
+    def memScan():
+        folder = filedialog.askdirectory()
+        #progress = tk.Tk()
+        #pb = ttk.Progressbar(progress, orient="horizontal", length=200, mode="indeterminate")
+        #pb.pack()
+        #pb.start()
+        dups = {}
+        wFolder = open(os.path.join('lists', "whitefolder.txt"),"a+")
+        wFolder.seek(0)
+        whiteFolder = [x.strip() for x in wFolder.readlines()]
+        wFolder.close()
+        folders = [folder]
+        for i in folders:
+            if i not in whiteFolder:
+                if os.path.exists(i):
+                    Hash.joinDicts(dups, Hash.memDup(i))
+        Hash.printResults(dups)
+
+    def scan():
         folder = filedialog.askdirectory()
         #progress = tk.Tk()
         #pb = ttk.Progressbar(progress, orient="horizontal", length=200, mode="indeterminate")
@@ -110,7 +140,7 @@ class Hash:
         folders = [folder]
         for i in folders:
             if os.path.exists(i):
-                Hash.joinDicts(dups, Hash.findDup(i))        
+                Hash.joinDicts(dups, Hash.findDup(i))
         Hash.printResults(dups)
 
 class Whitelist:
@@ -145,16 +175,24 @@ class Whitelist:
             print (whiteFolder + " has been whitelisted.")
         wFolder.close()
 
-class Options:
-    def check():
-        nothing = 1
+#class Realtime:
+#    
+#    def watchPath():
+#       insert = "here"
+#
+#    def realTimeWatch():
+#        insert = "here"
+
+#class Options:
+#    def check():
+#        nothing = 1
         
             
 class Main:       
     def main():
         result = messagebox.askyesno("DDPS","Would you like to perform another scan?")
         if result == True:
-            Hash.Scan()
+            Hash.memScan()
         else:
             sys.exit()
         Main.main()
@@ -164,7 +202,8 @@ if __name__ == "__main__":
     root.withdraw()
     result = messagebox.askyesno("DDPS","Would you like to perform a scan?")
     if result == True:
-        Hash.Scan()
+        Hash.memScan()
     else:
         sys.exit()
     Main.main()
+
