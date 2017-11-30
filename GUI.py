b34705276b58a0fa10921f50dd3c3fd7de41874f
ISRT5I <33969724+ISRT5I@users.noreply.github.com>
2017-11-29 22:37:25 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,36 +1,53 @@
 from tkinter  import *
+from PIL import Image, ImageTk
+import tkinter.ttk as ttk
 
 
-class GUI:
+class GUI(Frame):
 
-    def __init__(self, master):
-       frame = Frame(master)
-       frame.pack()
-       self.menuButton = Button(frame, text="Similar Scan", height=5, width=10)
-       self.menuButton.grid(row=0, sticky=W)
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+        self.pack(fill=BOTH, expand=1)
 
-       self.fileButton = Button(frame, text="Duplicates", height=5, width=10)
-       self.fileButton.grid(row=1, sticky=W)
+    def init_window(self):
+        simButton = Button(self, text="Similar Scan", height=5, width=10)
+        simButton.place(x=0, y=110)
 
-       self.dupButton = Button(frame, text="Whitelist", height=5, width=10)
-       self.dupButton.grid(row=2, sticky=W)
+        dupButton = Button(self, text="Duplicates", height=5, width=10)
+        dupButton.place(x=0, y=195)
 
-       self.setButton = Button(frame, text="Custom Scans", height=5, width=10)
-       self.setButton.grid(row=3, sticky=W)
+        listButton = Button(self, text="Whitelist", height=5, width=10)
+        listButton.place(x=0, y=280)
 
-       self.setButton = Button(frame, text="Settings", height=5, width=10)
-       self.setButton.grid(row=4, sticky=W)
+        cusButton = Button(self, text="Custom Scans", height=5, width=10)
+        cusButton.place(x=0, y=365)
 
-       self.scanButton = Button(frame, text="Scan", bg="blue", fg="white", height=3,
-                                width=15)
-       self.scanButton.grid(row=4, column=5, sticky=E)
+        setButton = Button(self, text="Settings", height=3, width=10)
+        setButton.place(x=0, y=450)
 
-       self.dupLabel = Label(frame, text="Duplicates: ")
-       self.dupLabel.grid(row=2, column=3, sticky=W)
+        scanButton = Button(self, text="Scan", font=("Sans"), bg="Blue",
+                                                     fg="white", height=4, width=20)
+        scanButton.place(x=500, y=350)
+
+        dupLabel = Label(self, text="Duplicates:", font=("Sans", 40))
+        dupLabel.place(x=100, y=250)
+
+        load = Image.open('DDPS_logo128.png')
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=0, y=0)
+
+        progressBar = ttk.Progressbar(self, length=600, orient='horizontal', mode='determinate')
+        progressBar.place(x=150, y=150)
 
 
-root = Tk()
-object = GUI(root)
-root.title("Duplicate Detection and Prevention System")
-root.geometry("880x580")
-root.mainloop()
+
+gui = Tk()
+object = GUI(gui)
+gui.title("Duplicate Detection and Prevention System")
+gui.geometry("800x500")
+gui.resizable(False, False)
+gui.mainloop()
