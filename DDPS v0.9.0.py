6a2ccd112e13d99ecb40026bc14a58f531bb1e95
ISRT5I <33969724+ISRT5I@users.noreply.github.com>
2017-11-30 16:35:32 -0600

    Updated DDPS to v0.9.0
    Added GUI, gave command to scan. Need to implement duplicates tab, real-time, and whitelist with popups for directories. Need to create settings, custom scans, and similar scans

--- DDPS v0.9.0.py
+++ DDPS v0.9.0.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+import PIL.Image, PIL.ImageTk
 from tkinter import filedialog, messagebox, ttk
 from tkinter import *
-import os, sys, time, hashlib, threading
+import os, sys, time ,hashlib ,threading
 
 #class Scroll(object):
 #
@@ -12,24 +13,65 @@ import os, sys, time, hashlib, threading
 #        self.txt['font'] = ('consolas', '12')
 #        self.txt.pack(expand=True, fill='both')
 
-#class GUI:
-#
-#    def __init__(self, master):
-#       frame = Frame(master)
-#       frame.pack()
-#       self.menuButton = Button(frame, text="Menu", height=5, width=10)
-#       self.menuButton.grid(row=0, sticky=W)
-#       self.fileButton = Button(frame, text="Files", height=5, width=10)
-#       self.fileButton.grid(row=1, sticky=W)
-#       self.dupButton = Button(frame, text="Duplicates", height=5, width=10)
-#       self.dupButton.grid(row=2, sticky=W)
-#       self.setButton = Button(frame, text="Settings", height=5, width=10)
-#       self.setButton.grid(row=3, sticky=W)
-#       self.scanButton = Button(frame, text="Scan", bg="blue", fg="white", height=3,width=15)
-#       self.scanButton.grid(row=3, column=5, sticky=E)
-#       self.dupLabel = Label(frame, text="Duplicates: ")
-#       self.dupLabel.grid(row=2, column=3, sticky=W)
-       
+class GUI(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+        self.pack(fill=BOTH, expand=1)
+        self.configure(background='gray45')
+
+    def init_window(self):
+        simButton = Button(self, text="Similar Scan", bg = "gray60", height=4, width=11)
+        simButton.place(x=0, y=80)
+
+        dupButton = Button(self, text="Duplicates", bg = "gray60", height=4, width=11)
+        dupButton.place(x=0, y=150)
+
+        realButton = Button(self, text="Real-Time", bg = "gray60", height=4, width=11)
+        realButton.place(x=0, y=220)
+
+        listButton = Button(self, text="Whitelist", bg = "gray60", height=4, width=11)
+        listButton.place(x=0, y=290)
+
+        cusButton = Button(self, text="Custom Scan(s)", bg = "gray60", height=4, width=11)
+        cusButton.place(x=0, y=360)
+
+        setButton = Button(self, text="Settings", bg = "gray45", height=4, width=11)
+        setButton.place(x=0, y=430)
+
+        scanButton = Button(self, text="Scan", font=("Calibri"), bg="Red2",
+                                                     fg="white", height=4, width=20, command=Hash.scan)
+        scanButton.place(x=545, y=375)
+
+        dupLabel = Label(self, text="Duplicates:", font=("Calibri", 40), bg = "gray45")
+        dupLabel.place(x=100, y=250)
+
+        load = PIL.Image.open('DDPS_logo64.png')
+        render = PIL.ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=5, y=5)
+        img.configure(background='gray45')
+
+        s = ttk.Style()
+        s.theme_use('classic')
+        s.configure("red.Horizontal.TProgressbar", foreground='red2', background='red2')
+        progressBar = ttk.Progressbar(self, length=600, orient='horizontal', mode='indeterminate', style="red.Horizontal.TProgressbar", maximum=50)
+        progressBar.place(x=150, y=125)
+
+    def progress(state):
+        s = ttk.Style()
+        s.theme_use('classic')
+        s.configure("red.Horizontal.TProgressbar", foreground='red2', background='red2')
+        gui.progressBar = ttk.Progressbar(gui, length=600, orient='horizontal', mode='indeterminate', style="red.Horizontal.TProgressbar", maximum=50)
+        gui.progressBar.place(x=150, y=125)
+        if state == 1:
+            gui.progressBar.start()
+        else:
+            gui.progressBar.stop()
+        
 class Hash:
 
     def findDup(parentFolder):
@@ -110,14 +152,12 @@ class Hash:
  
         else:
             messagebox.showerror("DDPS", "No duplicate files found.")
+        GUI.progress(0)
     
     def memScan():
+        GUI.progress(1)
         folder = filedialog.askdirectory()
-        #progress = tk.Tk()
-        #pb = ttk.Progressbar(progress, orient="horizontal", length=200, mode="indeterminate")
-        #pb.pack()
-        #pb.start()
-        dups = {}
+        GUI.progress(1)
         wFolder = open(os.path.join('lists', "whitefolder.txt"),"a+")
         wFolder.seek(0)
         whiteFolder = [x.strip() for x in wFolder.readlines()]
@@ -130,11 +170,8 @@ class Hash:
         Hash.printResults(dups)
 
     def scan():
+        GUI.progress(1)
         folder = filedialog.askdirectory()
-        #progress = tk.Tk()
-        #pb = ttk.Progressbar(progress, orient="horizontal", length=200, mode="indeterminate")
-        #pb.pack()
-        #pb.start()
         dups = {}
         folders = [folder]
         for i in folders:
@@ -179,10 +216,12 @@ class Realtime:
     def start():
         realFolder = open(os.path.join('lists', "realfolder.txt"),"r")
         folder = realFolder.read()
+        print (folder)
         if folder != "":
             threading.Thread(target=Realtime.watch, args = (folder,)).start()
+        else:
+            print("Must set directory first")
         realFolder.close()
-        print("Must set directory first")
         
     def check():
         real = open(os.path.join('lists', "realtime.txt"),"r")
@@ -225,18 +264,17 @@ class Realtime:
 #class Options:
 #    def check():
 #        nothing = 1
-        
-class Main:       
-    def menu():
-        result = messagebox.askyesno("DDPS","Would you like to perform a scan?")
-        if result == True:
-            Hash.memScan()
-        else:
-            sys.exit()
-        Main.menu()
+             
+
+
             
 if __name__ == "__main__":
     root = tk.Tk()
     root.withdraw()
     Realtime.check()
-    Main.menu()
+    gui = tk.Toplevel()
+    object = GUI(gui)
+    gui.title("Duplicate Detection and Prevention System")
+    gui.geometry("800x500")
+    gui.resizable(False, False)
+    gui.mainloop()
